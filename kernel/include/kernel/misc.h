c3957f065e0af425595ac4998ba586173498439a
DarmorGamz <darren09@live.ca>
2024-01-30 14:00:53 -0500

    Source and header of misc functions (not yet)

---
+++ kernel/include/kernel/misc.h
@@ -0,0 +1,49 @@
+/*******************************************************************************
+*                           C O P Y R I G H T (c)
+*							  D A R M O R ™
+*						    All Rights Reserved
+*******************************************************************************
+* @file       misc.h
+* @copyright  COPYRIGHT (c) 2024 Darmor™. All rights reserved.
+* @author     Darren Morrison
+* @brief      Header file for miscellaneous functionality
+***************************************************************************** */
+#ifndef KERNEL_MISC_H
+#define KERNEL_MISC_H
+
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+
+/** INCLUDES ******************************************************************/
+
+
+/** COMMON CONSTANT DEFINITIONS ***********************************************/
+
+
+/** COMMON MACRO DEFINITIONS **************************************************/
+
+
+/** TYPEDEFS, STRUCTURES AND ENUMERATIONS *************************************/
+
+
+/** PUBLIC FUNCTION PROTOTYPES ************************************************/
+
+
+/** TYPEDEFS, STRUCTURES AND ENUMERATIONS *************************************/
+
+
+/** GLOBAL VARIABLES (SHARED ACROSS CODE MODULES) *****************************/
+
+
+/** DEBUG *********************************************************************/
+
+
+#ifdef __cplusplus
+}
+#endif
+
+
+#endif // KERNEL_MISC_H
