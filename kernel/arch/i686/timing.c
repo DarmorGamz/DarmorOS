d0a0294fea285d88d2cb3f4b7cc36a13d592b2c9
DarmorGamz <darren09@live.ca>
2024-01-30 14:00:39 -0500

    Skeleton for PIT timer and irq

--- kernel/arch/i686/timing.c
+++ kernel/arch/i686/timing.c
@@ -1,23 +1,55 @@
 #include <kernel/timing.h>
+#include <sys/io.h>
+
+#define PIT_CRYSTAL_HZ 1193180
+
+static uint32_t u32CPUSpeed = 0;
+
+enum {
+    PIT_IRQ = 0x20,      // IRQ line for PIT
+    PIT_CHANNEL0 = 0x40, // Channel 0 data port (read/write)
+    PIT_CHANNEL1 = 0x41, // Channel 1 data port (read/write)
+    PIT_CHANNEL2 = 0x42, // Channel 2 data port (read/write)
+    PIT_COMMAND = 0x43,  // Mode/Command register (write only, a read is ignored)
+} ePITCHANNELS; 
+
+void kernel_init_pit(uint32_t u32Frequency) {
+    uint16_t u16Divisor = PIT_CRYSTAL_HZ / u32Frequency;
+
+    outb(PIT_COMMAND, 0x36);
+    outb(PIT_CHANNEL0, (u16Divisor & 0xFF));
+    outb(PIT_CHANNEL0, (u16Divisor >> 8) & 0xFF);
+}
+
+uint32_t kernel_read_sys_time() {
+    uint32_t time = inb(PIT_CHANNEL0);
+
+    return time;
+}
 
 void kernel_delay(uint32_t u32Milliseconds) {
     // Assume a processor running at 1 MHz
-    volatile uint32_t u32Cycles = u32Milliseconds * 1000;
+    if (u32CPUSpeed == 0) get_cpu_speed();
 
+    volatile uint32_t u32Cycles = (u32Milliseconds * u32CPUSpeed) >> 1; // divide by 2 since we have 2 instructions per burn.
+    u32Cycles = u32Cycles / 10;
     while (u32Cycles > 0) {
         asm volatile("nop"); // (no-operation) instructions for busy-waiting
-        u32Cycles--;
+        u32Cycles--; // 1 Instruction.
     }
 }
 
 uint32_t get_cpu_speed() {
     uint32_t result;
+
     asm volatile(
-        "cpuid\n\t"
-        "movl %%edx, %0"
-        : "=r" (result)
-        : "a" (0x80000002)
-        : "%ebx", "%ecx", "%edx"
+        "cpuid\n\t"          // Execute CPUID instruction
+        "movl %%edx, %0"     // Move the value in edx to the result variable
+        : "=r" (result)      // Output: constraint for the result variable
+        : "a" (0x80000002)   // Input: constraint for the CPUID function (0x80000002)
+        : "%ebx", "%ecx", "%edx" // Clobbered registers (registers that the assembler should assume have been modified)
     );
+
+    u32CPUSpeed = result;
     return result;
 }
--- kernel/include/kernel/timing.h
+++ kernel/include/kernel/timing.h
@@ -1,9 +1,54 @@
+/*******************************************************************************
+*                           C O P Y R I G H T (c)
+*							  D A R M O R ™
+*						    All Rights Reserved
+*******************************************************************************
+* @file       timing.h
+* @copyright  COPYRIGHT (c) 2024 Darmor™. All rights reserved.
+* @author     Darren Morrison
+* @brief      Header file for time related control
+***************************************************************************** */
 #ifndef KERNEL_TIMING_H
 #define KERNEL_TIMING_H
 
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+
+/** INCLUDES ******************************************************************/
 #include <stdint.h>
 
-void kernel_delay(uint32_t milliseconds);
+
+/** COMMON CONSTANT DEFINITIONS ***********************************************/
+
+
+/** COMMON MACRO DEFINITIONS **************************************************/
+
+
+/** TYPEDEFS, STRUCTURES AND ENUMERATIONS *************************************/
+
+
+/** PUBLIC FUNCTION PROTOTYPES ************************************************/
+void kernel_init_pit(uint32_t);
+void kernel_delay(uint32_t);
 uint32_t get_cpu_speed();
+uint32_t kernel_read_sys_time();
+
+
+/** TYPEDEFS, STRUCTURES AND ENUMERATIONS *************************************/
+
+
+/** GLOBAL VARIABLES (SHARED ACROSS CODE MODULES) *****************************/
+
+
+/** DEBUG *********************************************************************/
+
+
+#ifdef __cplusplus
+}
+#endif
+
 
 #endif // KERNEL_TIMING_H
