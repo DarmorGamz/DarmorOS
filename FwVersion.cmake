1a30eaeb9b16009845c740b03f3b11b56bc3eb57
DarmorGamz <darren09@live.ca>
2024-01-30 13:58:37 -0500

    -Added FwVersion.cmake file for versionization.

---
+++ FwVersion.cmake
@@ -0,0 +1,23 @@
+# Define the firmware version variables
+set(FW_MAJOR_VERSION 0)
+set(FW_MINOR_VERSION 0)
+set(FW_MAINT_VERSION 2)
+
+#[[/******************************************************************************
+ * Version Notes
+ ******************************************************************************\
+  Version: 0.0.2-alpha
+ Date: Jan 30st, 2024
+ Details:
+ - Added debug log functionality.
+ - Added terminal text and background colors.
+ - Added terminal scrolling, and auto scroll when hitting a newline.
+ - Added PIT timer stub, and CPU clock cycle
+ - Added %d, %u modifiers for printf(), and added \n \0 support.
+ ------------------------------------------------------------------------------
+   Version: 0.0.1-alpha
+ Date: Jan 16st, 2024
+ Details:
+ - Initial build
+ ------------------------------------------------------------------------------
+ ******************************************************************************/]]
