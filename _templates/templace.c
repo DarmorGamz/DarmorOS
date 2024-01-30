25fc4aae7a0c7cb26c0a0565fd33f001c347b810
DarmorGamz <darren09@live.ca>
2024-01-30 13:57:28 -0500

    -Added template directory. Too lazy to create source and header doxgen comments

---
+++ _templates/templace.c
@@ -0,0 +1,30 @@
+/*******************************************************************************
+*                           C O P Y R I G H T (c)
+*							  D A R M O R ™
+*						    All Rights Reserved
+*******************************************************************************
+* @file       <template>.c
+* @copyright  COPYRIGHT (c) 2024 Darmor™. All rights reserved.
+* @author     Darren Morrison
+* @brief      Source file for <template> functionality
+***************************************************************************** */
+/** INCLUDES ******************************************************************/
+// #include <kernel/<template>.h>
+
+
+/** LOCAL (PRIVATE) CONSTANT AND MACRO DEFINITIONS ****************************/
+
+
+/** LOCAL (PRIVATE) TYPEDEFS, STRUCTURES AND ENUMERATIONS *********************/
+
+
+/** VARIABLES *****************************************************************/
+
+
+/** LOCAL (PRIVATE) FUNCTION PROTOTYPES ***************************************/
+
+
+/** PUBLIC FUNCTION IMPLEMENTATIONS *******************************************/
+
+
+/** LOCAL (PRIVATE) FUNCTION IMPLEMENTATIONS **********************************/
---
+++ _templates/template.h
@@ -0,0 +1,49 @@
+/*******************************************************************************
+*                           C O P Y R I G H T (c)
+*							  D A R M O R ™
+*						    All Rights Reserved
+*******************************************************************************
+* @file       <template>.h
+* @copyright  COPYRIGHT (c) 2024 Darmor™. All rights reserved.
+* @author     Darren Morrison
+* @brief      Header file for <template> functionality
+***************************************************************************** */
+#ifndef KERNEL_<template>_H
+#define KERNEL_<template>_H
+
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+
+/** INCLUDES ******************************************************************/
+
+
+/** COMMON CONSTANT DEFINITIONS ***********************************************/
+
+
+/** COMMON MACRO DEFINITIONS **************************************************/
+
+
+/** TYPEDEFS, STRUCTURES AND ENUMERATIONS *************************************/
+
+
+/** PUBLIC FUNCTION PROTOTYPES ************************************************/
+
+
+/** TYPEDEFS, STRUCTURES AND ENUMERATIONS *************************************/
+
+
+/** GLOBAL VARIABLES (SHARED ACROSS CODE MODULES) *****************************/
+
+
+/** DEBUG *********************************************************************/
+
+
+#ifdef __cplusplus
+}
+#endif
+
+
+#endif // KERNEL_<template>_H
